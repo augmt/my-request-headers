f6c286314d36d85013e82fe9ef4e19f538a34953
Hector Garcia <soseinhg3@gmail.com>
2016-09-30 16:29:34 +0000

    Refactor to ES6
    Switch testing suite from Mocha to tape

--- test/test.js
+++ test/test.js
@@ -1,34 +1,85 @@
 'use strict';
 
-const expect = require('chai').expect;
-const request = require('supertest');
+import test from 'tape';
+import { expect } from 'chai';
+import request from 'supertest';
+import app from './../src/app.js';
 
-const server = require('./../app.js').listen();
+const server = app.listen();
 
-describe('app', function () {
-  it('should respond with json', function (done) {
-    request(server)
-      .get('/')
-      .expect('Content-Type', 'application/json; charset=utf-8', done);
-  });
+test(app.name, (t) => {
+  const tests = [
+    (t) => {
+      request(server)
+        .get('/')
+        .expect('Content-Type', /json/)
+        .end((err) => t.ifError(err, 'content-type should be json'));
+    },
+    (t, headers) => {
+      const get = request(server).get('/');
 
-  it('should respond with all and only the correct keys', function (done) {
-    request(server)
-      .get('/')
-      .set({'accept-language': '', 'x-forwarded-for': ''})
-      .expect(function (res) {
-        expect(res.body).to.have.all.keys([
+      for (let header in headers) {
+        if (headers[header] === null) {
+          get.unset(header);
+        } else {
+          get.set(header, headers[header]);
+        }
+      }
+
+      get
+        .expect((res) => expect(res.body).to.have.all.keys([
           'ipaddress',
           'language',
           'software'
-        ]);
-      })
-      .end(done);
+        ]))
+        .end((err) => t.ifError(err, 'response properties should consist of all and only the specified properties'));
+    }
+  ];
+  const testCases = [
+    {
+      headers: {'User-Agent': null},
+      message: 'when headers are not set',
+      tests
+    },
+    {
+      headers: {
+        'Accept-Language': '',
+        'User-Agent': '',
+        'X-Forwarded-For': ''
+      },
+      message: 'when headers are empty',
+      tests
+    },
+    {
+      headers: {
+        'Accept-Language': 'en,en-us;q=0.7,nl;q=0.3',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:48.0) Gecko/20100101 Firefox/48.0',
+        'X-Forwarded-For': '141.81.129.241, 140.16.171.217, 71.145.61.198'
+      },
+      message: 'when headers are set',
+      tests: tests.concat([
+        (t, headers) => {
+          request(server)
+            .get('/')
+            .set(headers)
+            .expect((res) => expect(res.body).to.deep.equal({
+              ipaddress: '141.81.129.241',
+              language: 'en',
+              software: 'Windows 10'
+            }))
+            .end((err) => t.ifError(err, 'response values should be a parsing of the header\'s specified properties'));
+        }
+      ])
+    }
+  ];
+
+  testCases.forEach((testCase) => {
+    t.test(testCase.message, (t) => {
+      t.plan(testCase.tests.length);
+
+      testCase.tests.forEach((test) => test(t, testCase.headers));
+    });
   });
-
-  it('should respond with the client\'s IP address');
-
-  it('should respond with the client\'s preferred language');
-
-  it('should respond with the client\'s operating system');
 });
+
+test.onFinish(() => server.close());
